b509cae3042127d2978d74eaf46e6e4b0a3c7c70
Maha Amjad <maha.thetasolutions@outlook.com>
2021-09-18 17:50:34 +0500

    Comment added.

--- app/src/main/java/com/example/kotlinbatchaug2021/Model/Student.kt
+++ app/src/main/java/com/example/kotlinbatchaug2021/Model/Student.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinbatchaug2021.Model
-
+//Model is any entity which you use in your project
+//class of Student
 data class Student (
     val Name:String,
     val PhoneNumber:String,
--- app/src/main/java/com/example/kotlinbatchaug2021/studentlist/StudentListAdapter.kt
+++ app/src/main/java/com/example/kotlinbatchaug2021/studentlist/StudentListAdapter.kt
@@ -12,26 +12,42 @@ import kotlinx.android.synthetic.main.student_item_view.view.*
 class StudentListAdapter: RecyclerView.Adapter<StudentListAdapter.studentViewHolder>() {
 
    var studentlist:List<Student> = listOf()
+    //class for viewholder
     class studentViewHolder(view: View): RecyclerView.ViewHolder(view)
 
+    //function to create view holder
+    //viewholder is list item design/holder which will hold data for evry list item
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): studentViewHolder {
-         var view=LayoutInflater.from(parent.context).inflate(R.layout.student_item_view,parent,false)
+
+        //inflating list item design/xml
+         val view=LayoutInflater.from(parent.context).inflate(R.layout.student_item_view,parent,false)
+
+        //returning StudentViewholder class with view
            return studentViewHolder(view)
     }
 
+
+    //function to bind holder and list items with each other
     override fun onBindViewHolder(holder: studentViewHolder, position: Int) {
+
+        //variable to hold one item of list
        val student:Student=studentlist[position]
+
         holder.itemView.studentName.text=student.Name
         holder.itemView.department.text=student.Department
         holder.itemView.phoneNumber.text=student.PhoneNumber
 
     }
 
+    //function returning count in list items
     override fun getItemCount(): Int {
+        //returning size/count of list (size is function of list)
         return studentlist.size
     }
 
+    //function to get list from activity
     fun submitList(studentList: List<Student>) {
+        //list which we get from activity assigned to variable
         studentlist=studentList
     }
 
